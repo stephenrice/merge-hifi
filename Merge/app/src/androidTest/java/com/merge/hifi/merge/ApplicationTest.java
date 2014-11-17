89433c53a446b7f69e44a08babb4df00215ea89f
Stephen Rice <srice120@gmail.com>
2014-11-16 17:31:24 -0800

    created project :i

---
+++ Merge/app/src/androidTest/java/com/merge/hifi/merge/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.merge.hifi.merge;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
